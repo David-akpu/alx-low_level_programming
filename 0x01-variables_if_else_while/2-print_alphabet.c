5195f2936069c19cc6e8b8dcef0e82eb0b01e905
David-akpu <davidakpu5@gmail.com>
2023-06-19 11:01:47 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main: - print the alphebet
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
