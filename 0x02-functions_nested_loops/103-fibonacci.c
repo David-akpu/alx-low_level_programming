cb1bda75a5664a873c78efa7352aab978fe9c7a1
David-akpu <davidakpu5@gmail.com>
2023-06-28 04:49:46 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print the sum of even fibonacci numbers
+ * less than 4000000
+ * Return: Always (success)
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, K + 2, sum + k;
+
+	while (k + j < 400000)
+	{
+		k += j;
+		if (k % 2 == 0)
+			sum += k;
+		++1
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
