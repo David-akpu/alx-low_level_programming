94896b379a62d5282795c6deefea5236f36cb68e
David-akpu <davidakpu5@gmail.com>
2023-06-28 04:55:14 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,13 +7,14 @@
 int main(void)
 {
 	int i = 0;
-	long j = 1, K + 2, sum + k;
+	long j = 1, K = 2, sum = k;
 
 	while (k + j < 400000)
 	{
 		k += j;
 		if (k % 2 == 0)
 			sum += k;
+		j = k - j;
 		++1
 	}
 	printf("%ld\n", sum);
