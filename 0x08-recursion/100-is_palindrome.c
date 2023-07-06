d2322b46819a0882548e87a2f02e1cb0093d4c17
David-akpu <davidakpu5@gmail.com>
2023-07-06 16:10:34 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,7 +9,7 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 		return (0);
 	else
-		return (i + _strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 }
 /**
  * comparator - compares each characher of the string.
@@ -22,7 +22,7 @@ int comparator(char *s, int n1, int n2)
 {
 	if (*(s + n1) == *(s + n2))
 	{
-		if (n1 == n2 || n1 n2 + 1)
+		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
 		return (0 + comparator(s, n1 = 1, n2 - 1));
 	}
@@ -36,6 +36,6 @@ int comparator(char *s, int n1, int n2)
 int is_palindrome(char *s)
 {
 	if (*s == '\0')
-		return (i);
+		return (1);
 	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
